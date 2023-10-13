3e0873a9b45299b6e6696c92e74ce1fc5a3e47c3
Pedro Al Buchori <p5817305788@gmail.com>
2023-10-13 11:22:01 +0700

    authentication login dan folder admin lte

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view ('login');
+    }
+
+   public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('/admin');
+        }
+    
+        Session::flash('status', 'failed');
+        Session::flash('message', 'login gagal');
+
+        return redirect('/');
+
+    }
+}
